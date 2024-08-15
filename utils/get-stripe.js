19b8f5c671ce2763d380943ecd7a94be796034f9
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-15 16:35:16 -0500

    wip get function for stripe

---
+++ utils/get-stripe.js
@@ -0,0 +1,9 @@
+﻿import {loadStripe} from '@stripe/stripe-js'
+let stripePromise
+const getStripe = () => {
+    if (!stripePromise) {
+        stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY)
+    }
+    return stripePromise
+}
+export default getStripe
