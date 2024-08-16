114744b3cea66d7b8f66cd3e64c4003d5f81117b
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-16 12:57:20 -0500

    added buttons for strip checkout

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
-
 import PricingTabs from './pricingtabs';
 import Landing from './landing';
 import Features from "./features";
+
+export const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_sessions', {
+        method: 'POST',
+        headers: { origin: 'http://localhost:3000' },
+    });
+    const checkoutSessionJson = await checkoutSession.json();
+
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+        sessionId: checkoutSessionJson.id,
+    });
+
+    if (error) {
+        console.warn(error.message);
+    }
+};
 
 export default function Home() {
     return (
