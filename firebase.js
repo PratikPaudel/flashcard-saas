2ad1f84736cb06909c991c689666d328937254a9
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-15 21:49:32 -0500

    removed google analytics

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 ﻿// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
