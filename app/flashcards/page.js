47f17045af4ac58f3b5fc7b28b3d4637f0ea5e5b
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-18 17:17:16 -0500

    moved the flashcard to its own component

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -7,9 +7,10 @@ import { db } from '@/firebase';
 import { Container, Grid, Box, Typography, Card, CardActionArea, CardContent } from '@mui/material';
 import { useSearchParams, useRouter } from 'next/navigation';
 import CustomAppBar from "@/app/appbar";
+import Flashcard from "@/app/flashcard/page.js";
 
 export default function Flashcards() {
-    const { isLoaded, isSignedIn, user } = useUser();
+    const {isLoaded, isSignedIn, user} = useUser();
     const [collections, setCollections] = useState([]);
     const [flashcards, setFlashcards] = useState([]);
     const [flipped, setFlipped] = useState({});
@@ -30,6 +31,7 @@ export default function Flashcards() {
                 setCollections(data.flashcards || []);
             }
         }
+
         getCollections();
     }, [user]);
 
@@ -43,10 +45,11 @@ export default function Flashcards() {
             const flashcards = [];
 
             docs.forEach((doc) => {
-                flashcards.push({ id: doc.id, ...doc.data() });
+                flashcards.push({id: doc.id, ...doc.data()});
             });
             setFlashcards(flashcards);
         }
+
         getFlashcards();
     }, [user, search]);
 
@@ -67,90 +70,31 @@ export default function Flashcards() {
 
     return (
         <>
-            <CustomAppBar />
-        <Container maxWidth="md">
-            {!search ? (
-                <Box sx={{ mt: 4, mb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Typography variant="h4">Your Flashcard Collections</Typography>
-                    <Grid container spacing={3} sx={{ mt: 4 }}>
-                        {collections.map((collection, index) => (
-                            <Grid item xs={12} sm={6} md={4} key={index}>
-                                <Card>
-                                    <CardActionArea onClick={() => handleCollectionClick(collection.name)}>
-                                        <CardContent>
-                                            <Typography variant="h5" component="div">
-                                                {collection.name}
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-            ) : (
-                <Container maxWidth="100vw">
-                    <Grid container spacing={3} sx={{ mt: 4 }}>
-                        {flashcards.map((flashcard, index) => (
-                            <Grid item xs={12} sm={6} md={4} key={index}>
-                                <Card>
-                                    <CardActionArea onClick={() => handleCardClick(index)}>
-                                        <CardContent sx={{background: "linear-gradient(#F9EFAF, #F7E98D)",}}>
-                                            <Box
-                                                sx={{
-                                                    perspective: "1000px",
-                                                    "& > div": {
-                                                        transition: "transform 0.6s",
-                                                        transformStyle: "preserve-3d",
-                                                        position: "relative",
-                                                        width: "100%",
-                                                        height: "200px",
-                                                        transform: flipped[index] ? "rotateY(180deg)" : "rotateY(0deg)",
-                                                    },
-                                                    "& > div > div": {
-                                                        position: "absolute",
-                                                        width: "100%",
-                                                        height: "200px",
-                                                        backfaceVisibility: "hidden",
-                                                        display: "flex",
-                                                        justifyContent: "center",
-                                                        alignItems: "center",
-                                                        padding: 2,
-                                                        boxSizing: "border-box",
-                                                    },
-                                                    "& > div > div:nth-of-type(1)": {
-                                                        visibility: flipped[index] ? "hidden" : "visible",
-                                                    },
-                                                    "& > div > div:nth-of-type(2)": {
-                                                        transform: "rotateY(180deg)",
-                                                        visibility: flipped[index] ? "visible" : "hidden",
-                                                    },
-                                                }}
-                                            >
-                                                <div>
-                                                    <div>
-                                                        <Typography variant="h5" component="div" sx={{ fontFamily: 'Gloria Hallelujah, cursive', fontSize: "1.0rem", background: "linear-gradient(#F9EFAF, #F7E98D)" }}>
-                                                            {flashcard.front}
-                                                        </Typography>
-                                                    </div>
-                                                    <div>
-                                                        <Typography variant="h5" component="div" sx={{ fontFamily: 'Gloria Hallelujah, cursive', fontSize: "1.0rem",  background: "linear-gradient(#F9EFAF, #F7E98D)"  }}>
-                                                            {flashcard.back}
-                                                        </Typography>
-                                                    </div>
-                                                </div>
-                                            </Box>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            )}
-        </Container>
-            </>
+            <CustomAppBar/>
+            <Container maxWidth="md">
+                {!search ? (
+                    <Box sx={{mt: 4, mb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <Typography variant="h4">Your Flashcard Collections</Typography>
+                        <Grid container spacing={3} sx={{mt: 4}}>
+                            {collections.map((collection, index) => (
+                                <Grid item xs={12} sm={6} md={4} key={index}>
+                                    <Card>
+                                        <CardActionArea onClick={() => handleCollectionClick(collection.name)}>
+                                            <CardContent>
+                                                <Typography variant="h5" component="div">
+                                                    {collection.name}
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                ) : (
+                    <Flashcard/>
+                )}
+            </Container>
+        </>
     );
 }
-
-
