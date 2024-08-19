e2f7812ababbef4abbb0d672dabcbcbb3034276a
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-08-18 23:56:41 -0500

    added buttons to go back, and to generate page

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -4,13 +4,14 @@ import { useUser } from '@clerk/nextjs';
 import { useEffect, useState } from 'react';
 import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '@/firebase';
-import { Container, Grid, Box, Typography, Card, CardActionArea, CardContent } from '@mui/material';
+import {Container, Grid, Box, Typography, Card, CardActionArea, CardContent, Button} from '@mui/material';
 import { useSearchParams, useRouter } from 'next/navigation';
 import CustomAppBar from "@/app/appbar";
 import Flashcard from "@/app/flashcard/page.js";
-
+import styles from './CardStyles.module.css';
+import ArrowBack from "@mui/icons-material/ArrowBack";
 export default function Flashcards() {
-    const {isLoaded, isSignedIn, user} = useUser();
+    const { isLoaded, isSignedIn, user } = useUser();
     const [collections, setCollections] = useState([]);
     const [flashcards, setFlashcards] = useState([]);
     const [flipped, setFlipped] = useState({});
@@ -45,7 +46,7 @@ export default function Flashcards() {
             const flashcards = [];
 
             docs.forEach((doc) => {
-                flashcards.push({id: doc.id, ...doc.data()});
+                flashcards.push({ id: doc.id, ...doc.data() });
             });
             setFlashcards(flashcards);
         }
@@ -70,29 +71,103 @@ export default function Flashcards() {
 
     return (
         <>
-            <CustomAppBar/>
+            <CustomAppBar />
+            {!search && (
+                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+                    <Button
+                        variant="contained"
+                        sx={{ fontFamily: 'Kalam, cursive', mt: 5, mb: 2, color: 'primary'}}
+                        onClick={() => router.back()}
+                    >
+                        <ArrowBack />
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        sx={{ fontFamily: 'Kalam, cursive', mt: 5, mb: 2 }}
+                        onClick={() => router.push('/generate')}
+                    >
+                        Create Flashcards
+                    </Button>
+                </Box>
+            )}
+
             <Container maxWidth="md">
                 {!search ? (
-                    <Box sx={{mt: 4, mb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <Box sx={{ mt: 4, mb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                         <Typography variant="h4">Your Flashcard Collections</Typography>
-                        <Grid container spacing={3} sx={{mt: 4}}>
+                        <Grid container spacing={3} sx={{ mt: 4 }}>
                             {collections.map((collection, index) => (
                                 <Grid item xs={12} sm={6} md={4} key={index}>
-                                    <Card>
-                                        <CardActionArea onClick={() => handleCollectionClick(collection.name)}>
-                                            <CardContent>
-                                                <Typography variant="h5" component="div">
-                                                    {collection.name}
-                                                </Typography>
-                                            </CardContent>
-                                        </CardActionArea>
-                                    </Card>
+                                    <Box sx={{ position: 'relative', width: '100%', height: '100px', padding: '5rem' }} onClick={() => handleCollectionClick(collection.name)}>
+                                        <Card
+                                            sx={{
+                                                position: 'absolute',
+                                                top: '0',
+                                                left: '0',
+                                                height: '100px',
+                                                width: '100%',
+                                                backgroundColor: '#F9F6EE',
+                                                zIndex: 0,
+                                                marginX: '-2rem',
+                                                boxShadow: 'none',
+                                                cursor: 'pointer',
+                                            }}
+                                        >
+                                            <CardActionArea>
+                                                <CardContent></CardContent>
+                                            </CardActionArea>
+                                        </Card>
+
+                                        <Card
+                                            sx={{
+                                                position: 'absolute',
+                                                top: '10px',
+                                                left: '10px',
+                                                height: '100px',
+                                                width: '100%',
+                                                backgroundColor: '#E2DFD2',
+                                                zIndex: 1,
+                                                boxShadow: 'none',
+                                                marginX: '-2rem',
+                                                cursor: 'pointer',
+                                            }}
+                                        >
+                                            <CardActionArea>
+                                                <CardContent></CardContent>
+                                            </CardActionArea>
+                                        </Card>
+
+                                        {/* Top Layer */}
+                                        <Card
+                                            sx={{
+                                                position: 'absolute',
+                                                top: '20px',
+                                                left: '20px',
+                                                height: '100px',
+                                                width: '100%',
+                                                backgroundColor: '#EDEADE',
+                                                zIndex: 2,
+                                                marginX: '-2rem',
+                                                boxShadow: 'none',
+                                                cursor: 'pointer',
+                                            }}
+                                        >
+                                            <CardActionArea>
+                                                <CardContent>
+                                                    <Typography variant="h5" component="div">
+                                                        {collection.name}
+                                                    </Typography>
+                                                </CardContent>
+                                            </CardActionArea>
+                                        </Card>
+                                    </Box>
                                 </Grid>
                             ))}
                         </Grid>
                     </Box>
                 ) : (
-                    <Flashcard/>
+                    <Flashcard />
                 )}
             </Container>
         </>
